8e83ba4042f51fe98ea9581446771b382868a43d
niyolynate <98440370+niyolynate@users.noreply.github.com>
2022-05-20 09:23:59 +0300

    Update app.js
    Web-App that creates Lists

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
+import Reactfrom 'react'
+
+import Header from'./components/Header'
+
 function App() {
-    const name = 'Brad'
-    const x = true
     return (
-        <div className='container'>
-            <h1>Hello from Home</h1>
-            <h2>Today is Work Day</h2>
-            <h3>Get Loving now</h3>
-            <Body>
-                <P>Goods and people have become way too happy douing nothing. Excellent workis still on high demand though. </p>
-            
-        <ul>
-            <l1>
-                <l1>Movement to Work</l1>
-                <l1>Snake Bites</l1>
-                <l1>Mothers in Labor</l1>
-                <l1>Books more popular</l1>
-                </ul>
-                </Body>
-            </div>
-    )
-}export default
+        <div.className='Container'>
+        <header/>
+        class app extends React.component {
+            render() {
+                return <h1>Task Manager</h1>
+                className.
+            }
+        }
